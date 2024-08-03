41ea217f635737891eafeefbb2367ad38d3e435c
ire-design <irenemwikalii04@gmail.com>
2024-08-03 23:22:17 +0300

    import  useState and useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import AccountContainer from "./AccountContainer";
 
 function App() {
+  
+
+
   return (
     <div className="ui raised segment">
       <div className="ui segment violet inverted">
